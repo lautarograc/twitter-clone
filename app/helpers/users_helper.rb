06f7dfb9f516816fe267821a50e4f9e506f0665e
lautarograc <lautarograciani2106@gmail.com>
2022-01-18 17:27:56 -0300

    fixed helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
-module UserHelper
+module UsersHelper
     def user_gravatar(user)
-      gravatar_id = Digest::MD5::hexdigest(user.eamil.downcase)
-      gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
-      image_tag(gravatar_url, alt: user.name)
+      if user_signed_in?
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+        image_tag(gravatar_url, alt: user.name)
+      end
     end
   end
