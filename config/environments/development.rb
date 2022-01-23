2786f35cdaf8489ca6329b29237574c76c0bae65
lautarograc <lautarograciani2106@gmail.com>
2022-01-22 22:31:12 -0300

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   #devise default url
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'twitter.graciani.ar'}
 
 
 end
